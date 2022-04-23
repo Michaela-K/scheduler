ab4590168377774c49fa3ae19259f625f455c0d8
Michaela-K <michaelakodes@gmail.com>
2022-04-23 14:57:21 -0400

    add conditional rendering to InterviewListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -2,17 +2,27 @@ import React from 'react';
 import classNames from 'classnames';
 import 'components/InterviewerListItem.scss';
 
-export default function InterviewerListItem() {
- return (
-  <>
-   <li className="interviewers__item">
-    <img
-     className="interviewers__item-image"
-     src="https://i.imgur.com/LpaY82x.png"
-     alt="Sylvia Palmer"
-    />
-    Sylvia Palmer
-   </li>
-  </>
+export default function InterviewerListItem(props) {
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+
+  const displayName = (props)=>{
+    if(props.selected){
+      return props.name
+    }
+  }
+
+  return (
+    <>
+    <li className={interviewerClass} onClick={()=>props.setInterviewer(props.id)}>
+      <img
+      className="interviewers__item-image"
+      src={props.avatar}
+      alt={props.name}
+      />
+      {displayName(props)}
+    </li>
+    </>
  );
 }
