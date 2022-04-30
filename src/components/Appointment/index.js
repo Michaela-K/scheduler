172ae8d785b1a7989e43dff5375c5d2f235eb001
Michaela-K <michaelakodes@gmail.com>
2022-04-30 11:16:47 -0400

    insert header story and component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 // import React from 'react';
-import 'Appointment/styles.scss'
+import './styles.scss'
 
 export default function Appointment(props) {
   return (
