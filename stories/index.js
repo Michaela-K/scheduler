2b044ab294a04e6b4b73c407fc82276d3c5ee3c7
Michaela-K <michaelakodes@gmail.com>
2022-05-05 14:00:44 -0400

    add confirm component and story

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from 'components/Appointment/index';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty'
 import Show from 'components/Appointment/Show'
+import Confirm from 'components/Appointment/Confirm' 
 
 //Button
 storiesOf("Button", module)
@@ -155,4 +156,4 @@ storiesOf("Button", module)
         .add("Header", () => <Header time="12pm" />) // the rest of your code should go here);
         .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
         .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
-      
+        .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm ={action("onConfirm")} onCancel={action("onCancel")} />)
