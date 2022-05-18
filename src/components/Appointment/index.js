9c13bb0bac76f177ca0366ccf3a7996882e61597
Michaela-K <michaelakodes@gmail.com>
2022-05-18 08:01:41 -0400

    error handling

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,6 +7,7 @@ import Show from 'components/Appointment/Show';
 import Form from './Form';
 import Status from './Status';
 import Confirm from './Confirm';
+import Error from './Error';
 import useVisualMode from 'hooks/useVisualMode';
 
 const EMPTY = 'EMPTY';
@@ -15,6 +16,9 @@ const CREATE = 'CREATE';
 const SAVING = 'SAVING';
 const DELETE = 'DELETE';
 const CONFIRM ='CONFIRM';
+const EDIT = 'EDIT';
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
 
 export default function Appointment(props) {
  const { mode, transition, back } = useVisualMode(
@@ -45,18 +49,21 @@ export default function Appointment(props) {
     // console.log("abc")
     transition(SHOW); 
   }) 
-  .catch((err) => console.log("error", err)); 
+  // .catch((err) => console.log("error", err)); 
+  .catch(() => transition(ERROR_SAVE, true)) //When we transition from SAVING to ERROR_SAVE we replace the mode in the history rather than pushing it on to the end.
  }
+ //We are creating functions for SAVING and ERROR_SAVE in the same scope. When we do this, they use the same transition function with the same history value.
 
  function deleteApp(id, interview){
   // transition(CONFIRM)
-  transition(DELETE)
+  transition(DELETE);
   props.cancelInterview(props.id, props.interview)
   .then(() => {
     console.log("123")
     transition(EMPTY)
-  }) 
-  console.log("INDEX.JS INTERVIEW ",props.interview)
+  })
+  // console.log("INDEX.JS INTERVIEW ",props.interview)
+  .catch(() => transition(ERROR_DELETE, true)) 
  }
 
  return (
@@ -73,6 +80,7 @@ export default function Appointment(props) {
       student={props.interview && props.interview.student}
       interviewer={props.interview && props.interview.interviewer}
       onDelete={()=> transition(CONFIRM)}
+      onEdit={() => transition(EDIT)}
      />
     )}
     {mode === CREATE && <Form interviewers={props.interviewers} onSave={save} onCancel={back}/>}
@@ -82,6 +90,19 @@ export default function Appointment(props) {
           message="Are you sure you would like to delete?"
           onCancel={back}
           onConfirm={deleteApp}/>}
+    {mode === EDIT && <Form student={props.interview.student} interviewers={props.interviewers} onSave={save} onCancel={back}/>}
+    {mode === ERROR_SAVE &&
+        <Error
+          onClose={back}
+          message="Your request could not be completed"
+        />
+      }
+      {mode === ERROR_DELETE &&
+        <Error
+          onClose={back}
+          message="Your request could not be completed"
+        />
+      }
    </article>
   </Fragment>
  );
