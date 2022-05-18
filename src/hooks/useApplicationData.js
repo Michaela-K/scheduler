1c6709132a2397ed428706f8d3ee338585d30272
Michaela-K <michaelakodes@gmail.com>
2022-05-18 13:45:54 -0400

    spots remaining/

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+//separating the rendering concern from the state management concern in our application.
+
 function useApplicationData() {
 
   const [state, setState] = useState({
@@ -29,6 +31,33 @@ function useApplicationData() {
    }, []);
 
 
+  const updateSpots = (state, appointments, id) => {
+    console.log("UAD state", state)
+    console.log("UAD Appointments", appointments)
+    console.log("UAD Id", id)
+    let spots = 0;
+    //get day
+    const dayObj = state.days.find(d => d.name === state.day);
+    console.log("UAD dayObj", dayObj)
+    //get id/index
+    console.log("UAD dayObj.appointments", dayObj.appointments)
+    for (const item of dayObj.appointments) {
+      console.log("UAD All Appointments", appointments)//all the appointments
+      const appointment = appointments[item] // specific appointment using item as index/id
+      console.log("appointments[item]", appointment) //each appointment object
+      if (!appointment.interview) {
+        spots++;
+      }
+    }
+    const newDay = { ...dayObj, spots } //creating a new obj - with dayObj created, and spots
+    console.log("newDay", newDay)
+    console.log("spots", spots)
+    const newDays = state.days.map(d => d.name === state.day ? newDay : d)
+    //if they both equal to Monday then newDays = newDay Object created
+    console.log("newDays", newDays)
+    return newDays;
+  };
+
  const setDay = (day) => setState((prev) => ({ ...prev, day }));
   
   function bookInterview(id, interview) {
@@ -44,6 +73,7 @@ function useApplicationData() {
      .put(`http://localhost:8001/api/appointments/${id}`, { interview })
      .then(() => {
       setState({ ...state, appointments });
+      days: updateSpots(state, appointments,id)
       // console.log(state);
      });
    }
@@ -64,6 +94,7 @@ function useApplicationData() {
      .then(() => {
       console.log("axios test")
       setState({ ...state, appointments });
+      days: updateSpots(state, appointments, id)
       console.log("Axios cancelInterview", state.appointments[`${id}`]);
      });
     }
