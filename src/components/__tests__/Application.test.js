95d27846df34dca08820eff04cc524bc25f1dab4
Michaela-K <michaelakodes@gmail.com>
2022-05-23 08:31:21 -0400

    mock axios

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent, waitForElement } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-xit("renders without crashing", () => {
-  render(<Application />);
-});
+// describe("Application", () => {
+  // xit("renders without crashing", () => {
+  //   render(<Application />);
+  // });
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText("Monday")).then(() => { //wait until we are able to get a DOM element with the text "Monday"
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    })
+  })
+// });
